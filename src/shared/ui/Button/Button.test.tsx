c4fcfda012bdd4f53b0321b7450597c111b8e41f
sxtscks <cvj.sp.bk@gmail.com>
2023-02-16 23:12:41 +0300

    delete debug

--- src/shared/ui/Button/Button.test.tsx
+++ src/shared/ui/Button/Button.test.tsx
@@ -10,6 +10,5 @@ describe('button', () => {
   test('with theme', () => {
     render(<Button theme={ThemeButton.CLEAR}>TEST</Button>);
     expect(screen.getByText('TEST')).toHaveClass('clear');
-    screen.debug();
   });
 });
