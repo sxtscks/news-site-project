3a3cf80694cac368da7f08f3acf419928c511e55
sxtscks <cvj.sp.bk@gmail.com>
2023-02-19 12:09:25 +0300

    fix test

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
@@ -1,17 +1,17 @@
 import { fireEvent, screen } from '@testing-library/react';
 import {
-  renderWithTranslations,
-} from 'shared/lib/tests/renderWithTranslations/renderWithTranslations';
+  componentRender,
+} from 'shared/lib/tests/componentRender/componentRender';
 import { Sidebar } from './Sidebar';
 
 describe('sidebar', () => {
   test('render', () => {
-    renderWithTranslations(<Sidebar />);
+    componentRender(<Sidebar />);
     expect(screen.getByTestId('sidebar')).toBeInTheDocument();
   });
 
   test('toggle', () => {
-    renderWithTranslations(<Sidebar />);
+    componentRender(<Sidebar />);
     const toggleButton = screen.getByTestId('sidebar-toggle');
     expect(screen.getByTestId('sidebar')).toBeInTheDocument();
     fireEvent.click(toggleButton);
