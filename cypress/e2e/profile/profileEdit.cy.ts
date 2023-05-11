f7559837d7a961658768401b3d86077d30479465
sxtscks <cvj.sp.bk@gmail.com>
2023-05-11 20:55:43 +0300

    add some new e2e tests

---
+++ cypress/e2e/profile/profileEdit.cy.ts
@@ -0,0 +1,21 @@
+let profileId: string;
+describe('Пользователь заходит на страницу профиля', () => {
+  beforeEach(() => {
+    cy.login('admin', '123').then((data) => {
+      profileId = data.id;
+      cy.visit(`profile/${data.id}`);
+    });
+  });
+  afterEach(() => {
+    cy.resetProfile(profileId);
+  });
+  it('И профиль успешно загружается', () => {
+    cy.getByTestId('ProfileCard.FirstnameInput').should('have.value', 'admin');
+    cy.getByTestId('ProfileCard.LastnameInput').should('have.value', 'admin');
+  });
+  it('И редактирует его', () => {
+    cy.updateProfile('new name', 'new lastname');
+    cy.getByTestId('ProfileCard.FirstnameInput').should('have.value', 'new name');
+    cy.getByTestId('ProfileCard.LastnameInput').should('have.value', 'new lastname');
+  });
+});
---
+++ cypress/support/commands/profile.ts
@@ -0,0 +1,32 @@
+export const updateProfile = (firstName: string, lastName: string) => {
+  cy.getByTestId('EditableProfileCardHeader.EditButton').click();
+  cy.getByTestId('ProfileCard.FirstnameInput').clear().type(firstName);
+  cy.getByTestId('ProfileCard.LastnameInput').clear().type(lastName);
+  cy.getByTestId('EditableProfileCardHeader.SaveButton').click();
+};
+
+export const resetProfile = (profileId: string) => cy.request({
+  method: 'PUT',
+  url: `http://localhost:8000/profile/${profileId}`,
+  headers: { Authorization: 'asasf' },
+  body: {
+    id: '1',
+    firstname: 'admin',
+    lastname: 'admin',
+    age: 465,
+    currency: 'RUB',
+    country: 'Russia',
+    city: 'Moscow',
+    username: 'admin',
+    avatar: 'https://teknotower.com/wp-content/uploads/2020/11/js.png',
+  },
+});
+
+declare global {
+  namespace Cypress {
+    interface Chainable {
+      updateProfile(firstName: string, lastName: string): Chainable<void>
+      resetProfile(profileId: string): Chainable<void>
+    }
+  }
+}
