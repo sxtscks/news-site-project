c277617b89a9e3b408f25d9caee6f8b064cf730c
sxtscks <cvj.sp.bk@gmail.com>
2023-05-11 16:18:40 +0300

    update dependencies

---
+++ config/storybook/main.ts
@@ -0,0 +1,65 @@
+import { Configuration, DefinePlugin, RuleSetRule } from 'webpack';
+import path from 'path';
+import { BuildPaths } from '../build/types/config';
+import { buildCssLoaders } from '../build/loaders/buildCssLoaders';
+
+export default {
+  stories: [
+    '../../src/**/*.stories.@(js|jsx|ts|tsx)',
+  ],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+    'storybook-css-modules',
+    'storybook-addon-mock',
+  ],
+  framework: '@storybook/react',
+  core: {
+    builder: 'webpack5',
+  },
+  webpackFinal: async (config: Configuration) => {
+    const paths: BuildPaths = {
+      build: '',
+      html: '',
+      entry: '',
+      src: path.resolve(__dirname, '..', '..', 'src'),
+      locales: '',
+      buildLocales: '',
+    };
+    config!.resolve!.modules!.push(paths.src);
+    config!.resolve!.extensions!.push('.ts', '.tsx');
+
+    // @ts-ignore
+    config!.module!.rules = config!.module!.rules!.map((rule: RuleSetRule) => {
+      if (/svg/.test(rule.test as string)) {
+        return { ...rule, exclude: /\.svg$/i };
+      }
+
+      return rule;
+    });
+
+    config!.module!.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+    config!.module!.rules.push(buildCssLoaders(true));
+
+    config!.plugins!.push(new DefinePlugin({
+      __IS_DEV__: JSON.stringify(true),
+      __API__: JSON.stringify('https://testapi.ru'),
+      __PROJECT__: JSON.stringify('storybook'),
+    }));
+
+    config!.resolve!.modules = [
+      path.resolve(__dirname, '../../src'),
+      'node_modules',
+    ];
+
+    config!.resolve!.alias = {
+      '@': path.resolve(__dirname, '..', '..', 'src'),
+    };
+
+    return config;
+  },
+};
